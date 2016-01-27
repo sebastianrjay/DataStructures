c189bae66d1bb28307ff41297cd85f96a09e2e4e
Sebastian Jay <sebastianrjay@gmail.com>
2016-01-27 12:49:58 -0800

    Add Ruby DoublyLinkedList

---
+++ doubly_linked_list.rb
@@ -0,0 +1,62 @@
+require 'set'
+
+class DoublyLinkedListNode
+	attr_accessor :next, :prev
+	attr_reader :value
+
+	def initialize(value)
+		@value = value
+	end
+end
+
+class DoublyLinkedList
+	attr_accessor :head, :tail
+
+	def initialize(node_values_array)
+		@nodes, @node_value_counts, previous_node = Set.new, Hash.new(0), nil
+
+		node_values_array.each_with_index do |node_value, i|
+			current_node = DoublyLinkedListNode.new(node_value)
+			@nodes << current_node
+
+			if i == 0
+				@head = current_node
+			elsif i == node_values_array.length - 1
+				@tail = current_node
+			end
+
+			if previous_node
+				current_node.prev = previous_node
+				previous_node.next = current_node
+			end
+
+			@node_value_counts[node_value] += 1	
+			previous_node = current_node
+		end
+	end
+
+	def node_count(value = nil)
+		return @nodes.count unless value
+
+		@node_value_counts[value]
+	end
+
+	def remove(node)
+		return nil unless @nodes.include?(node)
+		@node_value_counts[node.value] -= 1
+		@nodes.delete(node)
+
+		if node == tail
+			node.prev.next = nil
+			@tail = node.prev
+		elsif node == head
+			@head = node.next
+			node.next.prev = nil
+		else
+			node.prev.next = node.next
+			node.next.prev = node.prev
+		end
+
+		node
+	end
+end
